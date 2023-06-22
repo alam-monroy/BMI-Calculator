e865e44649770f9808c76adc6fea48d707e17439
Alam Monroy <106176434+alam-monroy@users.noreply.github.com>
2023-06-21 20:34:57 -0600

    initial commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by indra on 24/03/21.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
